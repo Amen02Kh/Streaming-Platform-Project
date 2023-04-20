abf448ee9ef6fe18656b702134442f5d82003c67
ghassen202 <ghassenmaiza1@gmail.com>
2023-04-20 01:33:33 +0100

    checkout animation and backend

---
+++ assets/php/checkout_form.php
@@ -0,0 +1,28 @@
+<?php
+
+$connection=mysqli_connect('localhost','root','','filmhub_db');
+
+if (isset($_POST['send'])) {
+
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $address=$_POST['address'];
+    $city=$_POST['city'];
+    $state=$_POST['state'];
+    $zipcode=$_POST['zipcode'];
+    $cardname=$_POST['cardname'];
+    $creditcard=$_POST['creditcard'];
+    $expm=$_POST['expm'];
+    $expy=$_POST['expy'];
+    $cvv=$_POST['cvv'];
+
+    $request="INSERT INTO payment(name,email,address,city,state,zipcode,cardname,creditcard,expm,expy,cvv) VALUES('$name','$email','$address','$city','$state','$zipcode','$cardname','$creditcard','$expm','$expy','$cvv')";
+
+    mysqli_query($connection,$request);
+    header('location:../../index.html');
+}
+else{
+    echo "Page d'erreur coocking ...";
+}
+
+?>
